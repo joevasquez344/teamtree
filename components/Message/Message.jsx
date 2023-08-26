2fa9d3e97f06ffc5abca114476bfaaf9a142de2a
Joe Vasquez <joevasquez344@gmail.com>
2023-08-25 20:58:40 -0700

    canceled hover effects on Message if screen size is over 768px

--- components/Message/Message.jsx
+++ components/Message/Message.jsx
@@ -42,15 +42,20 @@ const Message = ({
     if (inputEl === id) return;
     setHover(-1);
   };
+  const miniTablet = window.screen.width > 768;
 
   return (
     <div
       key={message.id}
-      onMouseOver={() => showMore(message.id)}
-      onMouseLeave={hideMore}
-      className={`group md:hover:bg-gray-900 ${message?.type === "task" && " "}  ${
+      onMouseOver={
+        window.screen.width > 768 ? () => showMore(message.id) : null
+      }
+      onMouseLeave={window.screen.width > 768 ? hideMore : null}
+      className={`group md:hover:bg-gray-900 ${
+        message?.type === "task" && " "
+      }  ${
         inputEl === message.id && "bg-gray-900"
-      } transition ease-in-out w-[80%] md:w-[90%] duration-200 px-3 md:px-5 py-1 md:py-3`}
+      } transition ease-in-out w-[80%] md:w-[100%] duration-200 px-3 md:px-5 py-1 md:py-3`}
     >
       {message.replyToMessage?.id && message.replyToText ? (
         <MessageReplyTo>
@@ -83,9 +88,11 @@ const Message = ({
               <div className="font-bold text-gray-300">
                 <div>{message?.user?.name}</div>
               </div>
-              <div className="text-xs md:text-sm text-gray-400">@{message?.user?.username}</div>
+              <div className="text-xs md:text-sm text-gray-400">
+                @{message?.user?.username}
+              </div>
             </div>
-            
+
             <div className="flex space-x-3 cursor-default">
               {inputEl === message.id ? (
                 <EditMessage
@@ -100,7 +107,9 @@ const Message = ({
                   {" "}
                   {message?.type === "task" ? (
                     <div className="border relative flex justify-between border-gray-600 group-hover:border group-hover:border-gray-700 my-2 w-full  p-3 rounded-md transition ease-in-out duration-200">
-                      <div className="text-sm md:text-base whitespace-break-spaces">{message.text}</div>
+                      <div className="text-sm md:text-base whitespace-break-spaces">
+                        {message.text}
+                      </div>
 
                       <div className="h-7 w-7 absolute -right-3 top-2.5  flex items-center justify-center rounded-full border bg-gray-700 group-hover:bg-gray-800 border-gray-500 transition ease-in-out duration-200">
                         <TaskIcon />

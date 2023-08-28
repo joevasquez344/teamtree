f6b07e61773d1215415f36b49e93ee6dee68ad23
Joe Vasquez <joevasquez344@gmail.com>
2023-08-27 18:29:30 -0700

    test 2

--- pages/teams/[teamId]/chat/index.jsx
+++ pages/teams/[teamId]/chat/index.jsx
@@ -145,7 +145,7 @@ const TeamChat = ({ mobileSidebarState }) => {
             membersMobile ? "-translate-x-[90%]  left-0 " : "left-0"
           }  ${
             mobileSidebarState ? "translate-x-[90%]  right-0 " : "right-0"
-          } col-span-12   md:col-span-8 lg:col-span-8   xl:col-span-9 2xl:col-span-10 bg-gray-700  overflow-y-scroll no-scrollbar  relative transition ease-in-out duration-300`}
+          } col-span-12   md:col-span-8 lg:col-span-8   xl:col-span-9 2xl:col-span-10 bg-gray-700 h-screen  overflow-y-scroll no-scrollbar  relative transition ease-in-out duration-300`}
         >
           <Chat
             messages={teamChat?.messages}
